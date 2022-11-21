0a5e132650c27f08400ee6ed23fde33aa153d8b5
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-11-21 17:55:23 -0500

    commit tree 6b1d628cd299d24a278be2a05e70b5daec16de5a

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    def name=(this_dogs_name)
+        @name=this_dogs_name
+    end
 
+    def breed=(dogs_breed)
+        @breed=dogs_breed
+    end
+
+    def name
+        @name
+    end
+    
+    def breed
+        @breed
+    end
 end
